79ae9d21d5ae94c1e1175acf49d74a1524a34ad8
Pradeep Korivi <korivi@campus.tu-berlin.de>
2019-01-13 00:09:13 +0100

    traffic light node updated, sometimes there is some initialization error but should be fixed

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -87,7 +87,7 @@ class WaypointUpdater(object):
 
     def waypoints_cb(self, waypoints):
         self.base_waypoints = waypoints
-        if not self.waypoints_2d:
+        if self.waypoints_2d is None:
             self.waypoints_2d = [[waypoint.pose.pose.position.x,waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
             print ("waypoints initialized")
